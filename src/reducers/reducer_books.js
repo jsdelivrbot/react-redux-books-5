e9921177c26da42f2bcde91136561535a83581bb
kctan <kctan>
2017-04-16 11:29:40 +0800

    redux books working version

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+    return [
+        {title: "Man called Ove", pages:101},
+        {title: "Eloquent Ruby", pages:85},
+        {title: "Whats yo dyde", pages:232},
+        {title: "My favorite book", pages:116}
+    ]
+}
+
